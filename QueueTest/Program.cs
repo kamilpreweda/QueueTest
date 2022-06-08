260d487bbfe72b129f5e7960f5cfe072f1dfd09d
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-08 09:26:26 +0200

    Added few examples

--- QueueTest/Program.cs
+++ QueueTest/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            Queue<string> myQueue = new Queue<string>();
+            myQueue.Enqueue("pierwszy w kolejce");
+            myQueue.Enqueue("drugi w kolejce");
+            myQueue.Enqueue("trzeci w kolejce");
+            myQueue.Enqueue("ostatni w kolejce");
+
+            // Metoda Peek "podgląda" pierwszy element bez usuwania go z kolejki.
+            Console.WriteLine($"Metoda Peek() zwróciła: \n{myQueue.Peek()}");
+
+            // Metoda Dequeue pobiera następny element z POCZĄTKU kolejki.
+            Console.WriteLine($"Pierwsze wywołanie Dequeue() zwróciło: \n{myQueue.Dequeue()}");
+            Console.WriteLine($"Drugie wywołanie Dequeue() zwróciło: \n{myQueue.Dequeue()}");
+
+            // Metoda Clear usuwa wszystkie elementy z kolejki.
+            Console.WriteLine($"Count przed wywołaniem Clear(): \n{myQueue.Count()}");
+            myQueue.Clear();
+            Console.WriteLine($"Count po wywołaniu Clear(): \n{myQueue.Count()}");
         }
     }
 }
